772eed577dee81d3b3ba4bc3870814c542c31a00
martinjoha <martinjohansen1705@gmail.com>
2018-09-09 15:56:10 +0200

    #5 page-container created components for general idea of the page. Pair programming with @kristogj

--- src/components/page-container.js
+++ src/components/page-container.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
+import ListComponent from "./list-component";
 
 class PageContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentListItem: "",
+            //will be the chosen category for the media.
+            graphic: "",
+            text: "",
+            sound: "",
+        }
+
+        this.onTabChange = this.onTabChange.bind(this);
+    }
+
+    onTabChange(listItem){
+        this.setState({
+            currentListItem: listItem
+            }
+        )
+    }
+
+
     render() {
         return(
-            <div>Container</div>
+            <div className="page-container">
+                <div>
+                    <ListComponent onTabCange={this.onTabChange} graphic={this.graphic} text={this.text} sound={this.sound} />
+                </div>
+            </div>
         )
     }
 }
 
 export default PageContainer;
+
