01ae45c313bbb18024c441e1a3d41315b29481df
martinjoha <martinjohansen1705@gmail.com>
2018-09-09 14:20:59 +0200

    #5 page-container
    Created new branch for creating the page container

---
+++ src/components/page-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PageContainer extends Component {
+    render() {
+        return(
+            <div>Container</div>
+        )
+    }
+}
+
+export default PageContainer;
